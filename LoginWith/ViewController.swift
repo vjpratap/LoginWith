e90f8ece7b4b897ccc25583f3d492cec6a887a62
Vijay Pratap Singh <vijaypratapsingh@invijayps-2.local>
2017-05-22 11:28:06 +0530

    Initial Commit

---
+++ LoginWith/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoginWith
+//
+//  Created by Vijay Pratap Singh on 5/22/17.
+//  Copyright © 2017 Vijay Pratap Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
