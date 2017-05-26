1cad9afdecc227cafffea3fb549b9d6cfa8528b3
Vijay Pratap Singh <vijaypratapsingh@invijayps-2.local>
2017-05-26 14:45:15 +0530

    [Vijay] | able to fetch user id when user login via facebook

--- LoginWith/ViewController.swift
+++ LoginWith/ViewController.swift
@@ -8,12 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
+    let loginButton: FBSDKLoginButton = {
+        let button = FBSDKLoginButton()
+        button.readPermissions = ["email"]
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(loginButton)
+        loginButton.center = view.center
+        loginButton.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        if let token = FBSDKAccessToken.current() {
+            fetchProfile()
+        }
     }
+    
+    func fetchProfile() {
+        print("fetch profile")
+        let parameters = ["fields": "email, first_name, last_name, picture"]
+        FBSDKGraphRequest(graphPath: "me", parameters: parameters).start { (connection, result, error) in
+            if error != nil {
+                print(error ?? "error")
+                return
+            }
+            
+            let resultValue = result as? NSDictionary
+            if let email = resultValue?["email"] as? String{
+                print(email)
+            }
+            
+        }
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        print("login complete")
+        fetchProfile()
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
+    
+    func loginButtonWillLogin(_ loginButton: FBSDKLoginButton!) -> Bool {
+        return true
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'LoginWith' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for LoginWith
+
+  target 'LoginWithTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'LoginWithUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
